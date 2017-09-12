d1f05e90dc51024dac7a6f91188ef501656517ec
Rodolfo Montes <rodolfo.montesgarza@gmail.com>
2017-09-11 22:02:29 -0500

    Adds controllers, models, and view folder, and user, transaction

---
+++ app/src/main/java/com/example/rodolfomg/proyecto/Models/Transactions.java
@@ -0,0 +1,64 @@
+package com.example.rodolfomg.proyecto.Models;
+
+import java.util.Date;
+
+/**
+ * Created by rodolfomg on 11/09/17.
+ */
+
+public class Transactions {
+    protected int id;
+    protected double amount;
+    protected Date date;
+    protected int user_id;
+    protected String title;
+    protected String description;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
---
+++ app/src/main/java/com/example/rodolfomg/proyecto/Models/User.java
@@ -0,0 +1,44 @@
+package com.example.rodolfomg.proyecto.Models;
+
+/**
+ * Created by rodolfomg on 11/09/17.
+ */
+
+public class User {
+    protected int id;
+    protected String username;
+    protected String password;
+    protected String email;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
