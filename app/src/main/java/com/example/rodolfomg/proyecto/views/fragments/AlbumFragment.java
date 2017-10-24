5835e353ac8a351971fa3982027a8c880f20a8e9
Rodolfo Montes <rodolfo.montesgarza@gmail.com>
2017-10-23 20:16:24 -0500

    Album Activity Created

--- app/src/main/java/com/example/rodolfomg/proyecto/views/fragments/AlbumFragment.java
+++ app/src/main/java/com/example/rodolfomg/proyecto/views/fragments/AlbumFragment.java
@@ -3,6 +3,8 @@ package com.example.rodolfomg.proyecto.views.fragments;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -21,8 +23,6 @@ public class AlbumFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle saveInstanceState){
         View view = inflater.inflate(R.layout.fragment_album, container, false);
         showToolbar("Album",false,view);
-
-
 
         return view;
     }
